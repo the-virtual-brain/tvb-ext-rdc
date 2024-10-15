992c94ad23a2cc182e912a398f2843d90cedcbd7
adrianciu <adrian.ciu@codemart.ro>
2024-10-15 17:19:58 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,48 @@
 import {
-    JupyterFrontEnd,
-    JupyterFrontEndPlugin
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import {ICommandPalette, MainAreaWidget} from '@jupyterlab/apputils';
-import {ILauncher} from '@jupyterlab/launcher';
-import IframeWidget from "./IframeWidget";
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
+import IframeWidget from './IframeWidget';
 
 const extension: JupyterFrontEndPlugin<void> = {
-    id: 'tvb-ext-rdc:plugin',
-    description: 'A JupyterLab extension which wraps a given URL as an env variable.',
-    autoStart: true,
-    requires: [ICommandPalette, ILauncher],
-    activate: (app: JupyterFrontEnd, commandPalette: ICommandPalette, launcher: ILauncher) => {
-        const commandId = 'iframe-widget:open';
-        app.commands.addCommand(commandId, {
-            label: 'Guacamole',
-            execute: () => {
-                const iframe = new IframeWidget('http://localhost:8080/guacamole/');
-                const widget = new MainAreaWidget({content: iframe});
-                widget.title.label = 'Guacamole';
-                widget.title.closable = true;
-                app.shell.add(widget, 'main');
-                app.shell.activateById(widget.id);
-            }
-        });
+  id: 'tvb-ext-rdc:plugin',
+  description:
+    'A JupyterLab extension which wraps a given URL as an env variable.',
+  autoStart: true,
+  requires: [ICommandPalette, ILauncher],
+  activate: (
+    app: JupyterFrontEnd,
+    commandPalette: ICommandPalette,
+    launcher: ILauncher
+  ) => {
+    const commandId = 'iframe-widget:open';
+    app.commands.addCommand(commandId, {
+      label: 'Guacamole',
+      execute: () => {
+        const iframe = new IframeWidget('http://localhost:8080/guacamole/');
+        const widget = new MainAreaWidget({ content: iframe });
+        widget.title.label = 'Guacamole';
+        widget.title.closable = true;
+        app.shell.add(widget, 'main');
+        app.shell.activateById(widget.id);
+      }
+    });
 
-        commandPalette.addItem({
-            command: commandId,
-            category: 'Guacamole'
-        });
-        if (launcher) {
-            launcher.add({
-                command: commandId,
-                category: 'Other',
-                rank: 1
-            });
-        }
+    commandPalette.addItem({
+      command: commandId,
+      category: 'Guacamole'
+    });
+    if (launcher) {
+      launcher.add({
+        command: commandId,
+        category: 'Other',
+        rank: 1
+      });
     }
+  }
 };
 
 export default extension;
