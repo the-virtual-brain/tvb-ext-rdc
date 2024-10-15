8d4ea7a0ab26c1019375f9710447cde060a84f2b
adrianciu <adrian.ciu@codemart.ro>
2024-10-15 16:59:54 +0300

    css for button

--- src/IframeWidget.tsx
+++ src/IframeWidget.tsx
@@ -1,57 +1,53 @@
 import * as React from 'react';
-import {ReactWidget} from '@jupyterlab/ui-components';
-import {useEffect, useState} from "react";
+import { ReactWidget } from '@jupyterlab/ui-components';
+import { useEffect, useState } from 'react';
 import '../style/IframeWidget.css';
 
 type IframeWidgetProps = {
-    initialUrl: string;
+  initialUrl: string;
 };
 
-const IframeWidgetComponent = ({initialUrl}: IframeWidgetProps) => {
-    const [url, setUrl] = useState<string>(initialUrl);
+const IframeWidgetComponent = ({ initialUrl }: IframeWidgetProps) => {
+  const [url, setUrl] = useState<string>(initialUrl);
 
-    const handleClick = () => {
-        setUrl('')
-    };
+  const handleClick = () => {
+    setUrl('');
+  };
 
-    useEffect(() => {
-        setUrl(initialUrl);
-    }, [url]);
+  useEffect(() => {
+    setUrl(initialUrl);
+  }, [url]);
 
-
-    return (
-        <div className="iframe-widget" style={{height: '100%', width: '100%'}}>
-            <div style={{marginBottom: '10px'}}>
-                <button
-                    className='custom-button'
-                    onClick={handleClick}
-                >
-                    Home
-                </button>
-            </div>
-            <iframe
-                src={url}
-                width="100%"
-                height="100%"
-                style={{border: 'none'}}
-                title="Iframe Display"
-                allow="clipboard-read; clipboard-write"
-            />
-        </div>
-    );
-}
+  return (
+    <div className="iframe-widget" style={{ height: '100%', width: '100%' }}>
+      <div style={{ marginBottom: '10px' }}>
+        <button className="button" onClick={handleClick}>
+          Home
+        </button>
+      </div>
+      <iframe
+        src={url}
+        width="100%"
+        height="100%"
+        style={{ border: 'none' }}
+        title="Iframe Display"
+        allow="clipboard-read; clipboard-write"
+      />
+    </div>
+  );
+};
 
 class IframeWidget extends ReactWidget {
-    private readonly url: string;
+  private readonly url: string;
 
-    constructor(url: string) {
-        super();
-        this.url = url;
-    }
+  constructor(url: string) {
+    super();
+    this.url = url;
+  }
 
-    render() {
-        return <IframeWidgetComponent initialUrl={this.url}/>;
-    }
+  render() {
+    return <IframeWidgetComponent initialUrl={this.url} />;
+  }
 }
 
-export default IframeWidget
+export default IframeWidget;
